80debd3b1f3e1a345bbe49f75d3916a96b302422
brugnoloJoao <brugnolojoao.31@gmail.com>
2026-05-05 15:06:12 -0300

    Criando Model Receitas

---
+++ K-Gest/Models/ReceitasViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace K_Gest.Models
+{
+    public class ReceitasViewModel
+    {
+        public int? IdSetor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Setor.")]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
